f54d1a140905b7c0fa549eb8fe42fd0ed7c206fb
palanik <palani.kumanan@gmail.com>
2015-03-04 21:17:55 -0500

    array test cases

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ var count = 20000;
 
 var blockingFunc = function(i, c) {
   var a = Array(1000).join(c);
-}
+};
 
 describe('blocking', function() {
   it('for loop', function(done) {
@@ -95,7 +95,7 @@ describe('increment', function() {
           );
   });
 
-  it('default value', function(done) {
+  it('default increment', function(done) {
     var sum = (count * (count + 1)) / 2;
     forloop(0, count,
       function(i) {
@@ -106,5 +106,125 @@ describe('increment', function() {
         done();
       }
           );
+  });
+});
+
+var golden = function(φ) {
+  return Math.sqrt(φ + Math.sqrt(1));
+};
+
+describe('forEach', function() {
+  it('array non-blocking', function(done) {
+    var blockErr = null;
+
+    blocked(function(ms){
+        blockErr = new Error("Blocked " + ms + "ms");
+      done(blockErr);
+    });
+
+    var a = Array(count);
+    var φ = 0;
+    forloop(a,
+      function(e, i) {
+        φ = golden(φ);
+        a[i] = φ;
+      },
+      function() {
+        if (!blockErr) {
+          done();
+        }
+      }
+          );
+  });
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+
+  it('array prime numbers', function(done) {
+
+    function isPrime(n) {
+      var i = 2;
+      while (i <= Math.sqrt(n)) {
+        if (n % i++ < 1) {
+          return false;
+        }
+      }
+      return n > 1;
+    }
+
+    function eratosthenes(arr, cb) {
+      arr[0] = arr[1] = true;
+      forloop(arr,
+              function(e, i, a) {
+                if (e === undefined) {
+                  forloop(i + i, a.length, i,
+                          function(j) {
+                            a[j] = false;
+                          });
+                  a[i] = true;
+                }
+              },
+              cb);
+    }
+
+    var validate = function(arr) {
+      forloop(arr,
+              function(e, i, a) {
+                if (i > 1) {
+                  assert.strictEqual(e, isPrime(i));
+                }
+              },
+              function(a) {
+                done();
+              });
+    };
+
+    eratosthenes(Array(count), validate);
+  });
+
+  it('array linear search', function(done) {
+
+    var find = function(arr, val, cb) {
+      var found = -1;
+
+      forloop(arr,
+        function(e, i) {
+          if (e == val) {
+            found = i;
+          }
+        },
+        function() {
+          cb(found);
+        }
+            );
+
+    };
+
+
+    var a = Array(count);
+
+    forloop(a,
+        // fill random values
+        function(e, i, arr) {
+          arr[i] = getRandomInt(0, arr.length);
+        },
+        function(arr) {
+          // Set array.length at a random location
+          var idx = getRandomInt(0, arr.length);
+          arr[idx] = arr.length;
+
+          // Find array.length at the specific location
+          find(arr, 
+                arr.length, 
+                function(found) {
+                  assert.equal(found, idx);
+                  done();
+                }
+              );
+        }
+            );
+
   });
 });
