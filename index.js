239e037ce88eef72800fefa13a4fa16e09284679
palanik <palani.kumanan@gmail.com>
2015-03-04 21:17:35 -0500

    loop arrays

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 // Copyright 2014 N. Palani Kumanan.  All rights reserved.
 
-function forloop(start, end, inc, iterator, callback) {
+function loop(check, iterator, done) {
+  // main function
+  var looper = function() {
+    // terminate?
+    if (check()) {
+      if (done) {
+        done();
+      }
+    }
+    else {
+      setImmediate(function() {
+        iterator();
+        looper();
+      });
+    }
+  };
+
+  // call it
+  looper();
+}
+
+function forloop() {
+  var arg0 = arguments[0];
+  if (Array.isArray(arg0)) {
+    return _forEach.apply(undefined, arguments);
+  }
+  return _forLoop.apply(this, arguments);
+}
+
+function _forLoop(start, end, inc, iterator, done) {
   var idx = start;
 
   // exit condition checking
@@ -10,7 +39,7 @@ function forloop(start, end, inc, iterator, callback) {
 
   if (typeof(inc) == 'function') {
     // inc missing, shift args to right
-    callback = iterator;
+    done = iterator;
     iterator = inc;
     inc = 1;
   }
@@ -19,29 +48,38 @@ function forloop(start, end, inc, iterator, callback) {
   if (inc < 0) {
     check = function() {
       return (idx <= end);
-    }
+    };
   }
 
-  // main function
-  var looper = function() {
-    // terminate?
-    if (check()) {
-      if (callback) {
-        callback();
-      }
-    }
-    else {
-      iterator(idx);
-      setImmediate(function() {
-        idx += inc;
-        looper();
-      });
-    }
-  }
+  var _it = function() {
+    iterator(idx);
+    idx += inc;
+  };
 
-  // call it
-  looper();
+  loop(check, _it, done);
 }
+
+function _forEach(obj, iterator, done) {
+  done = done || function(o) {};
+  if (Array.isArray(obj)) {
+    _forLoop(0, obj.length, 1, 
+            function(idx) {
+              iterator(obj[idx], idx, obj);
+            },
+            function() {
+              done(obj);
+            }
+            );
+  }
+}
+
+/*
+module.exports = {
+    forLoop: forLoop,
+    forEach: forEach,
+    version: '0.0.2'
+};
+*/
 
 module.exports = forloop;
 module.exports.version = '0.0.2';
